0c9f4461d249541f0076913b78b4f6f6b10e4775
Aminekb1 <amine.kbaier@esprit.tn>
2025-09-26 00:53:07 +0100

    edit jest config file

--- frontend_react/app/jest.config.cjs
+++ frontend_react/app/jest.config.cjs
@@ -2,4 +2,16 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],  
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx,js,jsx}",
+    "!**/node_modules/**",
+    "!**/dist/**",
+    "!**/coverage/**"
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text", "json", "clover", "cobertura"]
 };
