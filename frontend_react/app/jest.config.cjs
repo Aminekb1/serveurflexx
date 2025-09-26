88b6619aff52a690dd9ac278b3ed1adbbb16e1d9
Aminekb1 <amine.kbaier@esprit.tn>
2025-09-26 02:05:52 +0100

    debug

--- frontend_react/app/jest.config.cjs
+++ frontend_react/app/jest.config.cjs
@@ -1,11 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  setupFiles: ['<rootDir>/test-setup.js'],            // <-- IMPORTANT
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],  
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,   
   collectCoverageFrom: [
     "src/**/*.{ts,tsx,js,jsx}",
     "!**/node_modules/**",
@@ -13,5 +15,10 @@ module.exports = {
     "!**/coverage/**"
   ],
   coverageDirectory: "coverage",
-  coverageReporters: ["lcov", "text", "json", "clover", "cobertura"]
+  coverageReporters: ["lcov", "text", "json", "clover", "cobertura"],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js'
+  }
+
 };
